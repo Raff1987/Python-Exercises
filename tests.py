dda225902d6172f8b787ff5b15ca44b04f10f254
Raff1987 <36228792+Raff1987@users.noreply.github.com>
2018-02-07 19:25:39 +0000

    Update tests.py

--- tests.py
+++ tests.py
@@ -1,3 +1,52 @@
+
+#Week 1  Fibonacci
+
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+# Test the function with the following value.
+x = 15
+ans = fib(x)
+print("Fibonacci number", x, "is", ans)
+
+# Week 1
+def fib(n):
+  """This function returns the nth Fibonacci number."""
+  i = 0
+  j = 1
+  n = n - 1
+
+  while n >= 0:
+    i, j = j, i + j
+    n = n - 1
+  
+  return i
+
+name = "Martin"
+first = name[0]
+last = name[-1]
+firstno = ord(first)
+lastno = ord(last)
+x = firstno + lastno
+
+ans = fib(x)
+print("My surname is", name)
+print("The first letter", first, "is number", firstno)
+print("The last letter", last, "is number", lastno)
+print("Fibonacci number", x, "is", ans)
+
+
+# Week 2
+
 def fib(n):
   """This function returns the nth Fibonacci number."""
   i = 0
@@ -23,3 +72,10 @@ print("The first letter", first, "is number", firstno)
 print("The last letter", last, "is number", lastno)
 print("Fibonacci number", x, "is", ans)
 
+# My  name is Martin so M+N= 13+14 = 27. The 27th Fibonacci number is 196,418.
+# My surname is Rafter
+# The first letter R is number 82
+# The last letter r is number 114
+# Fibonacci number 196 is 40934782466626840596168752972961528246147
+# The defintion that makes sense to me at the moment is : 
+# Python has a function called ord() (short for ordinal) that returns a character as a number.
