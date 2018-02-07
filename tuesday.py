fe24abe92710a0fc217a77969c407a350c5fa51c
Raff1987 <36228792+Raff1987@users.noreply.github.com>
2018-02-07 19:45:10 +0000

    Add files via upload

---
+++ tuesday.py
@@ -0,0 +1,9 @@
+# Martin Rafter 5-2-18
+# is it tuesday
+
+import datetime
+
+if datetime.datetime.today().weekday() == 1:
+    print('yay! it is Tuesday.')
+else:
+    print('Unfortunaley it is not Tuesday')
