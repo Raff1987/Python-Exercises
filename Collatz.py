316b42a19e53f0ef304354fb0866c11dc0dd401a
Raff1987 <36228792+Raff1987@users.noreply.github.com>
2018-02-08 09:39:22 +0000

    Update Collatz.py

--- Collatz.py
+++ Collatz.py
@@ -1,15 +1,10 @@
 # Martin Rafter 5-2-18
 # The collatz conjecture program
-
 n = int(input("Integer:"))
-
 while n!= 1 : #  if answer is not equal to 1 keep running the loop 
-
     if n % 2 ==0: 
         n = n/2 #  if the number is even, the  number is divided by 2
-
         print (n) 
     else: # if the number is not even you multiple by 3 and add 1
-
         n = (n * 3) + 1
         print (round (n)) # The answer will always reach 1 no matter the number it starts at 
